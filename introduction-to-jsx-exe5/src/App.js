c16f69451aa5beb2683ed526dd010047f782ec69
Ochwada <nakhulolinda@yahoo.com>
2023-02-02 23:46:35 +0100

    exercise 5 - Webapp simple

--- introduction-to-jsx-exe5/src/App.js
+++ introduction-to-jsx-exe5/src/App.js
@@ -5,6 +5,10 @@ export default function App() {
     <div className="App">
       <h1>Welcome to Another Practice</h1>
       <h2>React Componet!</h2>
+
+      <div>
+        <p> Today is: {new Date().toLocaleString()} </p>
+      </div>
     </div>
   );
 }
