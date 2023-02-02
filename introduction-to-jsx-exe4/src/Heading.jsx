0054ef752223d27464ec36d7923d762f750cc5c5
Ochwada <nakhulolinda@yahoo.com>
2023-02-02 22:26:32 +0100

    exercise 4 - Webapp simple

---
+++ introduction-to-jsx-exe4/src/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//js
+
+function Heading() {
+  return <h1>Hello Ochwada!</h1>;
+}
+
+export default Heading;
---
+++ introduction-to-jsx-exe4/src/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+//js
+
+function List() {
+  return (
+    <ul>
+      <li>Paris</li>
+      <li>Belin</li>
+      <li>Rome</li>
+    </ul>
+  );
+}
+
+export default List;
