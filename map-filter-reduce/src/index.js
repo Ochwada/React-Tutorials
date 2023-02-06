7829b7a0c733cea5e87bb7cac6a1e205b60e76a0
Ochwada <nakhulolinda@yahoo.com>
2023-02-06 20:12:47 +0100

    Map-filter-reduce

--- map-filter-reduce/src/index.js
+++ map-filter-reduce/src/index.js
@@ -1,19 +1,32 @@
-import emojipedia from "./emojipedia";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
-const newEmojipedia = emojipedia.map((emojiEntry) => {
-    return emojiEntry.meaning.substring(0,100)
 
-});
+let numbers = [3, 56, 2, 48, 5];
 
-console.log(newEmojipedia);
-//Map -Create a new array by doing something with each item in an array.
+////Map -Create a new array by doing something with each item in an array.
+ const newNumbers = numbers.map( (x)=>  x * 2);
 
-
-//Filter - Create a new array by keeping the items that return true.
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+//   return num < 10;
+// });
 
 //Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// })
 
-//Find - find the first item that matches from an array.
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
 
-//FindIndex - find the index of the first item that matches.
+////FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
